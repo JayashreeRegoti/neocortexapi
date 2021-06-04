0cc8165eb1b6582aae418465dfff4fa21d2f862a
JayashreeRegoti3796 <84497715+JayashreeRegoti3796@users.noreply.github.com>
2021-06-04 13:13:30 +0200

    Create ImageConverter.cs
    read all the pixels of the image

---
+++ NeoCortexApi/NeoCortexApi.Experiments/ImageConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoCortexApi.Experiments
+{
+    public static class ImageConverter
+    {
+        public static int[,] BitmapToArray2D(Bitmap image)
+        {
+            int[,] array2D = new int[image.Width, image.Height];
+
+            BitmapData bitmapData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height),
+            ImageLockMode.ReadWrite,
+            PixelFormat.Format32bppRgb);
+
+            unsafe
+            {
+                byte* address = (byte*)bitmapData.Scan0;
+
+                int paddingOffset = bitmapData.Stride - (image.Width * 4);//4 bytes per pixel
+
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        byte[] temp = new byte[4];
+                        temp[0] = address[0];
+                        temp[1] = address[1];
+                        temp[2] = address[2];
+                        temp[3] = address[3];
+                        array2D[i, j] = BitConverter.ToInt32(temp, 0);
+                        ////array2D[i, j] = (int)address[0];
+
+                        //4 bytes per pixel
+                        address += 4;
+                    }
+
+                    address += paddingOffset;
+                }
+            }
+            image.UnlockBits(bitmapData);
+
+            return array2D;
+        }
+
+
+        //Working Correctly
+        public static Bitmap Array2DToBitmap(int[,] integers)
+        {
+            int width = integers.GetLength(0);
+            int height = integers.GetLength(1);
+
+            int stride = width * 4;//int == 4-bytes
+
+            Bitmap bitmap = null;
+
+            unsafe
+            {
+                fixed (int* intPtr = &integers[0, 0])
+                {
+                    bitmap = new Bitmap(width, height, stride, PixelFormat.Format32bppRgb, new IntPtr(intPtr));
+                }
+            }
+
+            return bitmap;
+        }
+    }
+}
