025733f97aa6a082b3bb1e0abe534247b4c7ad3a
Jayashree Regoti <56843949+JayashreeRegoti@users.noreply.github.com>
2023-12-25 13:35:03 +0100

    added basic projects

---
+++ source/Samples/NeoCortexApi.KnnSample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NeoCortexApi.Tools;
+
+Console.WriteLine("Hello, World!");
+await Run();
+
+async Task Run()
+{
+    await Task.Yield();
+    await ImageGenerator.CreateHorizontalImage();
+}
